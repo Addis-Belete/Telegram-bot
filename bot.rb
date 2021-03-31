f7606a578904035c01bc145ba2820f63450b822e
Addis Belete <addis.bel@gmail.com>
2021-03-31 14:00:05 +0300

    Add a menu module and include in bot file

--- bot.rb
+++ bot.rb
@@ -1,36 +1,40 @@
 require "telegram_bot"
+require_relative "./menu"
 
 class Delivery
-  token = "1609826675:AAHCcMOoLK6BJiW14IBBaWYsD9JeyRt4YSw"
-  bot = TelegramBot.new(token: token)
+  include Menu
 
-  bot.get_updates(fail_silently: true) do |message|
-    puts "@#{message.from.username}: #{message.text}"
-    command = message.get_command_for(bot)
+  def initialize
+    @token = "1609826675:AAHCcMOoLK6BJiW14IBBaWYsD9JeyRt4YSw"
+    @bot = TelegramBot.new(token: @token)
+  end
 
-    message.reply do |reply|
-      case command
-      when /start/i
-        reply.text = "Hello Welcome to Hideout cafe telegram bot. write /menu command to get the available food."
-      when /menu/i
-        reply.text = "Breakfast"
-        reply.text = "Launch"
-        reply.text = "Dinner"
-        reply.text = "Soft Drinks"
-        reply.text = "Hot-Drinks"
-      when /greet/i
-        greetings = ["bonjour", "hola", "hallo", "sveiki", "namaste", "salaam", "szia", "halo", "ciao"]
-        reply.text = "#{greetings.sample.capitalize}, #{message.from.first_name}!"
-      when /food/i
-        food = ["injera", "dabo", "duket"]
-        reply.text = "#{food.sample.capitalize}"
-      else
-        reply.text = "I have no idea what #{command.inspect} means."
+  def updates
+    @bot.get_updates(fail_silently: true) do |message|
+      puts "@#{message.from.username}: #{message.text}"
+      @command = message.get_command_for(@bot)
+
+      message.reply do |reply|
+        case @command
+        when /start/i
+          reply.text = "Hello Welcome to Hide out cafe telegram bot. write /menu command to get the menu of the items"
+        when /menu/i
+          reply.text = available_items
+        when /greet/i
+          greetings = ["bonjour", "hola", "hallo", "sveiki", "namaste", "salaam", "szia", "halo", "ciao"]
+          reply.text = "#{greetings.sample.capitalize}, #{message.from.first_name}!"
+        when /food/i
+          food = ["injera", "dabo", "duket"]
+          reply.text = "#{food.sample.capitalize}"
+        else
+          reply.text = "I have no idea what #{@command.inspect} means."
+        end
+        puts "sending #{reply.text.inspect} to @#{message.from.username}"
+        reply.send_with(@bot)
       end
-      puts "sending #{reply.text.inspect} to @#{message.from.username}"
-      reply.send_with(bot)
     end
   end
 end
 
 delivery = Delivery.new
+delivery.updates
---
+++ menu.rb
@@ -0,0 +1,6 @@
+module Menu
+  def available_items
+    return "For Hot drinks: Click /hot_drinks\nFor Cold  drinks: Click/cold_drinks\n
+For Food: Click/food"
+  end
+end
