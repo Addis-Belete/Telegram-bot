2bfa90d3ce0e608b47e14dc5ba5e78e55c7ad2dd
Addis Belete <addis.bel@gmail.com>
2021-03-31 14:50:50 +0300

    Add methods to return available items

--- menu.rb
+++ menu.rb
@@ -1,6 +1,18 @@
 module Menu
   def available_items
-    return "For Hot drinks: Click /hot_drinks\nFor Cold  drinks: Click/cold_drinks\n
-For Food: Click/food"
+    return 'For Hot drinks: Click "/hot_drinks"\nFor Cold  drinks: Click"/cold_drinks"\n
+For Food: Click"/food"'
+  end
+
+  def cold_drinks
+    return 'Cococala:/coca\n Pepsi:/pepsi \nWater:/water '
+  end
+
+  def soft_drinks
+    return ' Tea:/tea \n Macaito:/maciato Milk:/milk'
+  end
+
+  def food
+    return 'Beyeaynet/beyeaynet \n Burger:/burger Pizza:/pizza'
   end
 end
