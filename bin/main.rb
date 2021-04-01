9db463e0f9bc3d08c60ea0405f1bfeaed58e90ea
Addis Belete <addis.bel@gmail.com>
2021-04-01 16:27:02 +0300

    Fix offences

--- bin/main.rb
+++ bin/main.rb
@@ -1,8 +1,8 @@
-require_relative "../lib/bot"
-require "telegram/bot"
-token = "1743614866:AAGNJVfi5kFUY2938-OW3PloO-c4iyKZd1A"
+require_relative '../lib/bot'
+require 'telegram/bot'
+token = '1743614866:AAGNJVfi5kFUY2938-OW3PloO-c4iyKZd1A'
 Telegram::Bot::Client.run(token) do |bot|
-  puts "bot started"
+  puts 'bot started'
   bot.listen do |message|
     delivery = Delivery.new(message, bot)
     delivery.respond
