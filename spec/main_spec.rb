251d79255515d48315a0233bd56d54094ee03196
Addis Belete <addis.bel@gmail.com>
2021-04-01 14:39:48 +0300

    Add tests for menu

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -4,10 +4,23 @@ require "telegram/bot"
 
 describe Menu do
   describe "#list_of_selected_items" do
-    it "returns the string" do
+    it "returns the string with by numbering from 1" do
       delivery = Menu.new(@message, @bot)
       array = ["addis"]
       expect(delivery.list_of_selected_items(array)).to eql("1 : addis\n")
+    end
+  end
+  describe "#start" do
+    it "when start click it sends 'Welcome to hide out cafe'" do
+      delivery = Menu.new(@message, @bot)
+      expect(delivery.start).to eql("Welcome to hide out cafe")
+    end
+  end
+  describe "#available_items" do
+    it "when /menu typed it displays available items" do
+      delivery = Menu.new(@message, @bot)
+      expect(delivery.available_items).to eql("For Hot drinks: Click '/hot_drinks'\nFor Cold drinks: Click '/cold_drinks'\n
+For Food: Click '/food'")
     end
   end
 end
